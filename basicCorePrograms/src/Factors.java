67e384a2592f741db8795525561ab2fbe69fcb20
Kamalpreet19 <kk0924405@gmail.com>
2026-01-14 08:51:21 +0530

    Completed logic for Factors and HarmonicNumber

--- basicCorePrograms/src/Factors.java
+++ basicCorePrograms/src/Factors.java
@@ -4,5 +4,24 @@
 //c. Logic -> Traverse till i*i <= N instead of i <= N for efficiency.
 //d. O/P -> Print the prime factors of number N.
 
+
+import java.util.*;
 public class Factors {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter N:");
+        int N=sc.nextInt();
+
+        for(int i=2;i*i<=N;i++){
+            while(N%i==0){
+                System.out.print(i + " ");
+                N=N/i;
+            }
+        }
+        if(N>1){
+            System.out.println(N);
+        }
+
+    }
 }
--- basicCorePrograms/src/HarmonicNumber.java
+++ basicCorePrograms/src/HarmonicNumber.java
@@ -5,5 +5,26 @@
 //c. Logic -> compute 1/1 + 1/2 + 1/3 + ... + 1/N
 //d. O/P -> Print the Nth Harmonic Value.
 
+import java.util.*;
 public class HarmonicNumber {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the number:");
+        int num=sc.nextInt();
+
+        double result=0.0;
+
+        if(num<=0){
+            System.out.println("please enter a positive number");
+            return;
+        }
+        for(int i=1;i<=num;i++){
+            result=result+(double)1/i;
+
+        }
+        System.out.print("Result: " +result);
+
+
+    }
 }
