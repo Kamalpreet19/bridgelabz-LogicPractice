5f65a71fabd0b9b3fff0d4ed02fc8b92c3aa6c97
Kamalpreet19 <kk0924405@gmail.com>
2026-01-18 22:03:42 +0530

    UC2: Added functionality to add contact using console

--- PracticeProblemDayFiveOops/src/AddressBook.java
+++ PracticeProblemDayFiveOops/src/AddressBook.java
@@ -23,3 +23,43 @@ class Contact {
         this.email = email;
     }
 }
+public class AddressBook {
+
+    static ArrayList<Contact> contacts = new ArrayList<>();
+
+    public void addContact() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+
+        System.out.print("Enter Address: ");
+        String address = sc.nextLine();
+
+        System.out.print("Enter City: ");
+        String city = sc.nextLine();
+
+        System.out.print("Enter State: ");
+        String state = sc.nextLine();
+
+        System.out.print("Enter Zip: ");
+        String zip = sc.nextLine();
+
+        System.out.print("Enter Phone Number: ");
+        String phone = sc.nextLine();
+
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
+
+        Contact contact = new Contact(
+                firstName, lastName, address,
+                city, state, zip, phone, email
+        );
+
+        contacts.add(contact);
+        System.out.println("Contact Added Successfully!");
+    }
+}
